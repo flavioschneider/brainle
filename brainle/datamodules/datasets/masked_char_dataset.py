8988a6a110df93a4ebd7e3d11fc484088ebbec56
Flavio Schneider <flavio.schneider.97@gmail.com>
2022-02-27 21:14:22 +0100

    feat: added SM model, datamodule, exp text_000

---
+++ brainle/datamodules/datasets/masked_char_dataset.py
@@ -0,0 +1,61 @@
+from typing import List
+
+import numpy as np
+import torch
+from tokenizers import normalizers
+from tokenizers.normalizers import NFD, Lowercase, StripAccents
+from tokenizers.pre_tokenizers import Whitespace
+from torch.utils.data import Dataset
+
+
+class MaskedCharDataset(Dataset):
+    def __init__(
+        self, text: str, block_size: int, p_word_mask: float, p_char_mask: float
+    ):
+        normalizer = normalizers.Sequence([NFD(), StripAccents()])
+        text = normalizer.normalize_str(text)
+        text_length = len(text)
+        alphabet = sorted(list(set(text)))
+        print(f"Alphabet size {len(alphabet)}")
+
+        # Keep index 0 for mask
+        self.char_to_id = {char: idx + 1 for idx, char in enumerate(alphabet)}
+        self.id_to_char = {idx + 1: char for idx, char in enumerate(alphabet)}
+
+        self.block_size = block_size
+        self.alphabet = alphabet
+        self.text = text
+        self.p_word_mask = p_word_mask
+        self.p_char_mask = p_char_mask
+        self.pre_tokenizer = Whitespace()
+
+    def __len__(self):
+        return len(self.text) - self.block_size
+
+    def encode(self, text: str) -> List[int]:
+        return [self.char_to_id[char] for char in text]
+
+    def decode(self, ids: List[int]) -> str:
+        return "".join([self.id_to_char[idx] for idx in ids])
+
+    def __getitem__(self, idx):
+        chunk = self.text[idx : idx + self.block_size + 1]
+
+        x = torch.tensor(self.encode(chunk), dtype=torch.long)[0 : self.block_size]
+        mask = torch.ones_like(x)
+
+        # Mask random words
+        words = self.pre_tokenizer.pre_tokenize_str(chunk)
+        words_mask = np.random.binomial(1, self.p_word_mask, len(words))
+        for idx, word_triplet in enumerate(words):
+            word, (start, end) = word_triplet
+            if words_mask[idx]:
+                mask[start:end] = 0
+
+        # Mask random letters
+        chars_mask = torch.tensor(
+            np.random.binomial(1, self.p_char_mask, self.block_size)
+        )
+        mask[chars_mask == 1] = 0
+
+        return x, mask
---
+++ brainle/datamodules/wikitext_datamodule.py
@@ -0,0 +1,70 @@
+from typing import Any, Callable, Optional
+
+import pytorch_lightning as pl
+import torch
+import torchvision
+from datasets import load_dataset
+from torch.utils.data import DataLoader, random_split
+
+from .datasets.masked_char_dataset import MaskedCharDataset
+
+
+class WikiTextDatamodule(pl.LightningDataModule):
+    def __init__(
+        self,
+        batch_size: int,
+        train_val_split: tuple,
+        num_workers: int,
+        block_size: int,
+        p_word_mask: float,
+        p_char_mask: float,
+        version: str = "wikitext-2-raw-v1",
+        pin_memory: bool = False,
+    ):
+        super().__init__()
+        self.batch_size = batch_size
+        self.train_val_split = train_val_split
+        self.num_workers = num_workers
+        self.block_size = block_size
+        self.p_word_mask = p_word_mask
+        self.p_char_mask = p_char_mask
+        self.version = version
+        self.pin_memory = pin_memory
+        self.dataset_train: Any = None
+        self.dataset_valid: Any = None
+
+    def get_wikitext(self, split):
+        dataset = load_dataset("wikitext", self.version, split="train")
+        text = ""
+        for i in range(len(dataset)):
+            text += dataset[i]["text"]
+        return text
+
+    def setup(self, stage: Any = None) -> None:
+        dataset = MaskedCharDataset(
+            text=self.get_wikitext("train"),
+            block_size=self.block_size,
+            p_word_mask=self.p_word_mask,
+            p_char_mask=self.p_char_mask,
+        )
+        self.dataset_train, self.dataset_valid = random_split(
+            dataset, self.train_val_split
+        )
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(
+            dataset=self.dataset_train,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+            shuffle=True,
+        )
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(
+            dataset=self.dataset_valid,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+            shuffle=False,
+        )
