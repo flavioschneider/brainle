d2c141b65921d9ba8e6b5eec18e9371388f292da
Flavio Schneider <flavio.schneider.97@gmail.com>
2022-03-30 00:41:17 +0200

    fix: added dim check

--- brainle/models/architectures/knn.py
+++ brainle/models/architectures/knn.py
@@ -28,11 +28,15 @@ class KNNBlock(nn.Module):
         return self.index.ntotal
 
     def forward(self, x: Tensor, k: int) -> Tensor:
+        # Convert to numpy
         x_numpy = x.cpu().detach().numpy()
+        # Dimensionality check
+        n, d = x.shape
+        assert d == self.features, f"Expected tensor of shape [n, features]"
         # KNN search into index with k neighbors
         distances, indices, embedding = self.index.search_and_reconstruct(x_numpy, k)
         return {
             "distances": torch.tensor(distances).to(x),
             "indices": torch.tensor(indices).to(x),
-            "embedding": torch.tensor(embedding).to(x),
+            "embedding": torch.tensor(embedding).to(x),  # Shape [n, k, d]
         }
