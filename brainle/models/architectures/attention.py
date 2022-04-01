1d5b348c483c473a3914d2f66fda3c2b32c61efc
Flavio Schneider <flavio.schneider.97@gmail.com>
2022-04-02 01:23:38 +0200

    feat: added KVMemory GPU support

--- brainle/models/architectures/attention.py
+++ brainle/models/architectures/attention.py
@@ -809,7 +809,12 @@ class KVMemory(nn.Module):
         self.memory_size = memory_size
         self.items_per_query = items_per_query
         # Initialize index for KNN search and memory
-        self.index = faiss.IndexFlatIP(k_features)
+        if torch.cuda.is_available():
+            gpu_resource = faiss.StandardGpuResources()
+            index_cpu = faiss.IndexFlatIP(k_features)
+            self.index = faiss.index_cpu_to_gpu(gpu_resource, 0, index_cpu)
+        else:
+            self.index = faiss.IndexFlatIP(k_features)
         self.index.add(np.zeros((memory_size, k_features), dtype="float32"))
         self.register_buffer("k_memory", torch.zeros(memory_size, k_features))
         self.register_buffer("v_memory", torch.zeros(memory_size, v_features))
