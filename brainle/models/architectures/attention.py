eead2e3723037faa55dfd36316475d2a5832617b
Flavio Schneider <flavio.schneider.97@gmail.com>
2022-04-02 10:38:14 +0200

    fix: KVMemory faiss GPU

--- brainle/models/architectures/attention.py
+++ brainle/models/architectures/attention.py
@@ -811,11 +811,7 @@ class KVMemory(nn.Module):
         self.memory_size = memory_size
         self.items_per_query = items_per_query
         # Initialize index for KNN search and memory
-        if torch.cuda.is_available():
-            index_cpu = faiss.IndexFlatIP(k_features)
-            self.index = faiss.index_cpu_to_all_gpus(index_cpu)
-        else:
-            self.index = faiss.IndexFlatIP(k_features)
+        self.index = faiss.IndexFlatIP(k_features)
         self.index.add(np.zeros((memory_size, k_features), dtype="float32"))
         self.register_buffer("k_memory", torch.zeros(memory_size, k_features))
         self.register_buffer("v_memory", torch.zeros(memory_size, v_features))
